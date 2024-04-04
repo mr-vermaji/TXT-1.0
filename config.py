9b7f08a78dd641b1082dcd15fd69b2d6e474231e
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-04-04 23:09:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 28528007
+
+API_HASH = os.environ.get("API_HASH", "38464da16c80310cabc8d13952419cf3")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7031030184:AAHmGa2u2CR6hQgDrHxbUcKeaIkw_qrWlHA")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6717769514))
+
+LOG = -1002095973382
+
