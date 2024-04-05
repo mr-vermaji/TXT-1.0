f5bf384d949f07bf385b01b8a360f4af76de63cd
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-04-05 09:26:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ bot = Client("bot",
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"Hi 👋 Sir ! How are You Sir 🤓❤️?\n\n☞ I'm a High Speed **Txt File** Downloader Bot.\n\n☞ I can Download **Videos & Pdf** From Your **TXT** File.\n\n**𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 :** [『 𝗛𝗘𝗠𝗨 』]/n")
+    editable = await m.reply_text(f"Hi 👋 Sir ! How are You Sir 🤓❤️?\n\n☞ I'm a High Speed **Txt File** Downloader Bot.\n\n☞ I can Download **Videos & Pdf** From Your **TXT** File.\n\n**𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 :** 𝗛𝗘𝗠𝗨/n")
   
 
 @bot.on_message(filters.command("stop"))
